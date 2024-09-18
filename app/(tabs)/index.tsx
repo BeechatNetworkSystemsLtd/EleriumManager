cadf1fc9d48143d904e96e4d64be90156e07509b
Maksym Ostapenko <maksym.ostapenko@embrox.com>
2024-09-18 15:05:38 +0300

    chore: upload test code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,93 @@
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
-import { dqxPerformNFC } from '@beechatnetwork/elerium-lib';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+
+import { urlSignProgram, urlSignReset } from "@beechatnetwork/elerium-lib";
+import { Buffer } from "buffer";
+
+const { height: screenHeight } = Dimensions.get("window");
 
 export default function HomeScreen() {
-  const doSomething = async () => {
-    console.log(`[DEBUG]: aaa`)
-    await dqxPerformNFC('1', '2', '3');
+  const [password, setPassword] = useState("bonkadvi");
+  const [url, setUrl] = useState("streetmint.xyz/mint/3947857711");
+  const [pubKey, setPubKey] = useState(null);
+
+  const doProgram = async () => {
+    const pubKey = await urlSignProgram(password, url);
+    setPubKey(Buffer.from(pubKey).toString("hex"));
+  };
+  const doReset = async () => {
+    urlSignReset(password);
   };
 
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <TouchableOpacity onPress={doSomething} style={{justifyContent: 'center', padding: 40, borderColor: 'black', borderWidth: 1}}>
-        <Text>GO!</Text>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.textInput}
+        value={password}
+        onChangeText={setPassword}
+        placeholder="Password"
+      />
+      <TextInput
+        style={styles.textInput}
+        value={url}
+        onChangeText={setUrl}
+        placeholder="Url"
+      />
+
+      <TextInput
+        style={styles.textInput}
+        value={pubKey}
+        placeholder="Public Key"
+      />
+
+      <TouchableOpacity
+        onPress={async () => await doProgram()}
+        style={{
+          justifyContent: "center",
+          padding: 40,
+          marginBottom: 40,
+          borderColor: "black",
+          borderWidth: 1,
+        }}
+      >
+        <Text>Program</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={async () => await doReset()}
+        style={{
+          justifyContent: "center",
+          padding: 40,
+          borderColor: "black",
+          borderWidth: 1,
+        }}
+      >
+        <Text>Reset</Text>
       </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  textInput: {
+    width: "90%",
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 20,
+    textAlignVertical: "top", // Ensures the text starts at the top of the input
   },
 });
